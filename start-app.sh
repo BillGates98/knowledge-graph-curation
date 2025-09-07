88dbfa6d86e18388d89b89a6bc09c71fce08a193
BillGates98 <billhappi@gmail.com>
2025-09-08 00:57:41 +0200

    Readme updated

--- start-app.sh
+++ start-app.sh
@@ -1,3 +1,5 @@
+cd /tmp
+rm -rf knowledge-graph-curation knowledge-graph-curation-api
 git clone https://github.com/BillGates98/knowledge-graph-curation.git
 git clone https://github.com/BillGates98/knowledge-graph-curation-api.git
 docker-compose up --build
